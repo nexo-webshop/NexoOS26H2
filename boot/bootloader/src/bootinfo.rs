accc5545d7397bf2f73b330acb2ac35a84525bed
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-07-05 21:35:36 +0200

    Refactor BootInfo structure for memory map

--- boot/bootloader/src/bootinfo.rs
+++ boot/bootloader/src/bootinfo.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
 pub struct BootInfo {
-    pub memory_map_addr: usize,
-    pub memory_map_size: usize,
+    pub memory_map_ptr: usize,
+    pub memory_map_entries: usize,
     pub framebuffer_addr: usize,
     pub framebuffer_width: u32,
     pub framebuffer_height: u32,
