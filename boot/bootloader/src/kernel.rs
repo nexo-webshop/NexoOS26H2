a63b7c4449189736bdf44551509d6505b195d641
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-07-05 21:31:47 +0200

    Revise kernel loading function comment
    Updated version comment for kernel loading function.

--- boot/bootloader/src/kernel.rs
+++ boot/bootloader/src/kernel.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 
 pub fn load_kernel() -> Option<usize> {
-    // v0.2026.00003: alleen simulatie
+    // v0.2026.00004: nog steeds stub
+    // later vervangen door ELF loader
+
     let fake_kernel_entry: usize = 0x100000;
     Some(fake_kernel_entry)
 }
