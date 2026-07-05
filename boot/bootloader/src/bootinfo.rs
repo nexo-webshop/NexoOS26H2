df789f01a94bf96d13ac983eb56c79f6cea59650
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-07-05 21:38:13 +0200

    Update BootInfo structure with new memory fields
    Renamed memory_map_ptr to memory_map_addr and added usable_memory and reserved_memory fields.

--- boot/bootloader/src/bootinfo.rs
+++ boot/bootloader/src/bootinfo.rs
@@ -1,10 +1,15 @@
 #![allow(dead_code)]
 
 pub struct BootInfo {
-    pub memory_map_ptr: usize,
+    pub memory_map_addr: usize,
     pub memory_map_entries: usize,
+
+    pub usable_memory: usize,
+    pub reserved_memory: usize,
+
     pub framebuffer_addr: usize,
     pub framebuffer_width: u32,
     pub framebuffer_height: u32,
+
     pub kernel_entry: usize,
 }
