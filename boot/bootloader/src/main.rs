da9a2c7af057c11b10b3b1ab6b97a4c120c574f0
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-07-05 21:19:52 +0200

    Initialize UEFI and add bootloader status messages

--- boot/bootloader/src/main.rs
+++ boot/bootloader/src/main.rs
@@ -1,11 +1,16 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-
 use uefi::prelude::*;
+use uefi::println;
 
 #[entry]
 fn efi_main() -> Status {
+    uefi::helpers::init();
+
+    println!("NexoOS Bootloader v0.2026.00002");
+    println!("Status: OK");
+    println!("UEFI initialized");
+
     Status::SUCCESS
 }
